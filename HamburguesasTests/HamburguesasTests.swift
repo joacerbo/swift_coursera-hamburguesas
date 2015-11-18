ae101db686a7a9a799d2599337dc5be7ffd0a1a8
Joaquín Cerdá Boluda <joacerbo@macbook-air-de-joaquin.local>
2015-11-18 19:09:06 +0100

    Initial Commit

---
+++ HamburguesasTests/HamburguesasTests.swift
@@ -0,0 +1,36 @@
+//
+//  HamburguesasTests.swift
+//  HamburguesasTests
+//
+//  Created by Joaquín Cerdá Boluda on 18/11/15.
+//  Copyright © 2015 Ximo Cerdà. All rights reserved.
+//
+
+import XCTest
+@testable import Hamburguesas
+
+class HamburguesasTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
